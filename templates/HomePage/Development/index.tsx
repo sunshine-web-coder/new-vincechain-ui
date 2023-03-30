71add154e721cf3453182e5d082102d7d308bc44
sunshine-web-coder <webcoderdev02@gmail.com>
2023-03-30 18:27:46 +0100

    h

--- templates/HomePage/Development/index.tsx
+++ templates/HomePage/Development/index.tsx
@@ -26,7 +26,7 @@ const Development = ({ scrollToRef }: DevelopmentProps) => {
             ></div>
             <div className={cn("container-wide", styles.container)}>
                 <h2 className={cn("h2", styles.title)} style={{marginTop: 100}}>
-                The vince ecosystem.
+                The Vince Ecosystem.
                 </h2>
                 <div className={styles.list} {...events} ref={ref}>
                     {development.map((item, index) => (
