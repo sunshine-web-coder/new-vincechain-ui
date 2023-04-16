b0461d18faf62d97781c997501981f24e5abeacf
sunshine-web-coder <webcoderdev02@gmail.com>
2023-04-16 07:39:22 +0100

    xl

--- templates/SupportPage/index.tsx
+++ templates/SupportPage/index.tsx
@@ -3,11 +3,10 @@ import Layout from "@/components/Layout";
 import Main from "./Main";
 
 const SupportPage = () => {
-    const scrollToRef = useRef(null);
 
     return (
         <Layout>
-            <Main scrollToRef={scrollToRef} />
+            <Main />
         </Layout>
     );
 };
